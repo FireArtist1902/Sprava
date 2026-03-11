7f4ec27de1d4649b2792bb5d83134b1c7edfa323
Vadym <vadimvrachenko@gmail.com>
2026-03-11 21:23:09 +0200

    added Routes object

---
+++ app/src/main/java/com/example/sprava/navigation/Routes.kt
@@ -0,0 +1,8 @@
+package com.example.sprava.navigation
+
+object Routes {
+    const val HOME = "home"
+    const val CREATION = "creation"
+    const val SCHEDULE = "schedule"
+    const val SETTINGS = "settings"
+}
